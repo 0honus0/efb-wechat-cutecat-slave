4ae958471421b519e915d17e221591190bcfd871
QQ-War <2991889+QQ-War@users.noreply.github.com>
2022-04-17 11:14:30 +0800

    some small description modify

--- efb_wechat_slave/MsgDecorator.py
+++ efb_wechat_slave/MsgDecorator.py
@@ -125,7 +125,7 @@ def efb_share_link_wrapper(text: str) -> Tuple[Message]:
     //appmsg/type = 36 : 京东农场
     //appmsg/type = 51 : 视频（微信视频号分享）
     //appmsg/type = 57 : 【感谢 @honus 提供样本 xml】引用(回复)消息，未细致研究哪个参数是被引用的消息 id 
-    //appmsg/type = 63 : 直播（微信视频号）
+    //appmsg/type = 63 : 直播（微信视频号分享）
     //appmsg/type = 74 : 文件 (收到文件的第一个提示)
     :param text: The content of the message
     :return: EFB Message
@@ -289,7 +289,7 @@ def efb_share_link_wrapper(text: str) -> Tuple[Message]:
                     vendor_specific={ "is_wechatsport": True }
                 )
                 efb_msgs.append(efb_msg)
-        elif type == 51: # 微信视频号分享
+        elif type == 51: # 视频（微信视频号分享）
             title = xml.xpath('/msg/appmsg/title/text()')[0]
             imgurl = xml.xpath('/msg/appmsg/finderFeed/avatar/text()')[0].strip("<![CDATA[").strip("]]>")
             desc = xml.xpath('/msg/appmsg/finderFeed/desc/text()')[0]
@@ -316,7 +316,7 @@ def efb_share_link_wrapper(text: str) -> Tuple[Message]:
                 vendor_specific={ "is_refer": True }
             )
             efb_msgs.append(efb_msg)
-        elif type == 63: #视频号消息
+        elif type == 63: # 直播（微信视频号分享）
             title = xml.xpath('/msg/appmsg/title/text()')[0]
             imgurl = xml.xpath('/msg/appmsg/finderLive/headUrl/text()')[0].strip("<![CDATA[").strip("]]>")
             desc = xml.xpath('/msg/appmsg/finderLive/desc/text()')[0].strip("<![CDATA[").strip("]]>")
